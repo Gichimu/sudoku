7e12cfde25aa30bce8cc3364e845ef6794285ff9
Gichimu <gichimueric12@gmail.com>
2021-04-26 21:07:14 +0300

    additional styles

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const cell81 = document.getElementById("81");
+
+// cell81.addEventListener("click", () => {
+//     ('[data-toggle="popover"]').popover();
+// });
+
+// $(function () {
+//   $('[data-toggle="popover"]').popover();
+// });
