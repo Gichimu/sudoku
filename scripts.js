8815d4aa35f8ce8cc45ddfc822fe419190a73614
Gichimu <gichimueric12@gmail.com>
2021-04-27 21:42:07 +0300

    add table to popover

--- scripts.js
+++ scripts.js
@@ -1,9 +1,2 @@
-const cell81 = document.getElementById("81");
 
-cell81.addEventListener("click", () => {
-    ('[data-toggle="popover"]').popover();
-});
 
-// $(function () {
-//   $('[data-toggle="popover"]').popover();
-// });
